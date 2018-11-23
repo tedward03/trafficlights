eb29ed46551554c1dfec349bcbd2298cf0c2b210
tedward03 <tedward603@gmail.com>
2018-11-23 04:48:03 +0000

    forgot timings config javadoc

--- src/main/java/com/edxamples/trafficlights/config/TimingsConfig.java
+++ src/main/java/com/edxamples/trafficlights/config/TimingsConfig.java
@@ -4,6 +4,9 @@ import com.edxamples.trafficlights.shared.Timings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * This timings config is in a separate configuration class so I can easily override the bean in my test, by just not including it
+ */
 @Configuration
 public class TimingsConfig {
 
